11e5d815c1f43f0f7a570b3e5504e1353c50a1b4
jayeshb <bhangalejayesh2001@gmail.com>
2024-09-15 16:54:41 +0530

    main

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(
   cors({
     origin: ["http://localhost:3000", "http://localhost:3001", "https://project-management-solutions.web.app"],
-    methods: ["GET", "POST", "DELETE", "PUT"],
+    methods: ["GET", "POST", "DELETE", "PUT", "OPTIONS"],
     credentials: true,
   })
 );
